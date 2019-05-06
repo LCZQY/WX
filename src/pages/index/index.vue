94cbf39f7f4d5eb8e18ef57402ffafd02708074c
zqy <2536156085@QQ.com>
2019-05-06 09:56:19 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,7 +9,7 @@
         <card :text="userInfo.nickName"></card>
       </div>
     </div>
-
+    
     <div class="usermotto">
       <div class="user-motto">
         <card :text="motto"></card>
@@ -22,7 +22,7 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
 
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+    <a href="/pages/counter/main" class="counter">测试提交</a>
 
     <div class="all">
         <div class="left">
